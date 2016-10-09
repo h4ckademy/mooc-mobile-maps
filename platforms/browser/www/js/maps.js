b29ef9dd5bcdab260e6e3478ff4318219323f5d3
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2016-10-10 00:31:15 +0200

    geolocalización

--- platforms/browser/www/js/maps.js
+++ platforms/browser/www/js/maps.js
@@ -1,15 +1,33 @@
 var app = {
   inicio: function() {
     this.iniciaFastClick();
+
   },
 
   iniciaFastClick: function () {
     FastClick.attach(document.body);
+  },
+
+  dispositivoListo: function(){
+    navigator.geolocation.getCurrentPosition(app.dibujaCoordenadas, app.errorAlSolicitarLocalizacion);
+  },
+
+  dibujaCoordenadas: function(position){
+    var coordsDiv = document.querySelector('#coords');
+    coordsDiv.innerHTML = 'Latitud: ' + position.coords.latitude + ' Longitud:' + position.coords.longitude;
+  },
+
+  errorAlSolicitarLocalizacion: function(error){
+    console.log(error.code + ': ' + error.message);
   }
+
 };
 
 if ('addEventListener' in document) {
   document.addEventListener('DOMContentLoaded', function() {
     app.inicio();
+  }, false);
+  document.addEventListener('deviceready', function() {
+    app.dispositivoListo();
   }, false);
 }
