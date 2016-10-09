1fcb313f5f1d7f8017bc07f13e0127a27b976c37
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2016-10-10 00:56:18 +0200

    mapas con leaflet

--- platforms/browser/www/js/maps.js
+++ platforms/browser/www/js/maps.js
@@ -9,12 +9,16 @@ var app = {
   },
 
   dispositivoListo: function(){
-    navigator.geolocation.getCurrentPosition(app.dibujaCoordenadas, app.errorAlSolicitarLocalizacion);
+    navigator.geolocation.getCurrentPosition(app.pintaCoordenadasEnMapa, app.errorAlSolicitarLocalizacion);
   },
 
-  dibujaCoordenadas: function(position){
-    var coordsDiv = document.querySelector('#coords');
-    coordsDiv.innerHTML = 'Latitud: ' + position.coords.latitude + ' Longitud:' + position.coords.longitude;
+  pintaCoordenadasEnMapa: function(position){
+    var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ29vdHlmZXIiLCJhIjoiY2l1MGlrb2M3MDAwMDJ6bXAxY3dlOXdkYiJ9.RBfUsuzHfLrofEyMR8IVlA', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(miMapa);
   },
 
   errorAlSolicitarLocalizacion: function(error){
