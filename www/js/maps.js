5df698279b7fb52597244e7988c88fc7641dc79a
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2016-10-10 01:20:29 +0200

    marcadores en un mapa

--- www/js/maps.js
+++ www/js/maps.js
@@ -19,6 +19,18 @@ var app = {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
     }).addTo(miMapa);
+
+    app.pintaMarcador([position.coords.latitude, position.coords.longitude], '¡Estoy aquí!', miMapa);
+
+    miMapa.on('click', function(evento){
+      var texto = 'Marcador en l(' + evento.latlng.lat.toFixed(2) + ') y L(' + evento.latlng.lng.toFixed(2) + ')';
+      app.pintaMarcador(evento.latlng, texto, miMapa);
+    });
+  },
+
+  pintaMarcador: function(latlng, texto, mapa){
+    var marcador = L.marker(latlng).addTo(mapa);
+    marcador.bindPopup(texto).openPopup();
   },
 
   errorAlSolicitarLocalizacion: function(error){
